ee3fdf434c9b4e274f3ed0a3b13d52d003d60463
Nikolay Gromin <gromin@gmail.com>
2015-05-18 11:26:51 +0300

    Testing multiple dirs

--- index.js
+++ index.js
@@ -4,18 +4,30 @@ var path = require('path');
 var fs = require('fs');
 var mime = require('mime');
 
-module.exports = function (givenImagesPath) {
+function expandPath(file, givenPath) {
+    var searchPath;
+
+    if (!givenPath) {
+        searchPath = path.dirname(file.path);
+    } else {
+        if (path.resolve(givenPath) === path.normalize(givenPath)) {
+            searchPath = givenPath;
+        }
+        else {
+            searchPath = path.join(path.dirname(file.path), givenPath);
+        }
+    }
+
+    console.log(givenPath)
+    return searchPath;
+}
+
+module.exports = function (givenPaths) {
+
+    givenPaths = Array.isArray(givenPaths) ? givenPaths : [givenPaths];
+
     function base64Inline (file, enc, callback) {
         var imagesPath;
-
-        if (!givenImagesPath) {
-            imagesPath = path.dirname(file.path);
-        } else {
-            imagesPath = path.join(path.dirname(file.path), givenImagesPath);
-            if (path.resolve(givenImagesPath) === path.normalize(givenImagesPath)) {
-                imagesPath = givenImagesPath;
-            }
-        }
 
         // Do nothing if no contents
         if (file.isNull()) {
@@ -29,16 +41,37 @@ module.exports = function (givenImagesPath) {
             return callback();
         }
 
-        function inline (image, imagePath) {
-            var fileMime = mime.lookup(imagePath);
+        function inline (inlineExpr, quotedInlinePath) {
+            var inlinePath = quotedInlinePath.replace(/[\'\"]/g, '');
+            var fileMime = mime.lookup(inlinePath);
             var prefix = 'url(data:' + fileMime  + ';base64,';
-            var fileData = fs.readFileSync(path.join(imagesPath, imagePath));
-            return prefix + new Buffer(fileData).toString('base64') + ')';
+            var fileData;
+            var res;
+
+            givenPaths.forEach(function(givenPath) {
+                givenPath = expandPath(file, givenPath);
+                try {
+                    fileData = fs.readFileSync(path.join(givenPath, inlinePath));
+                }
+                catch (e) {
+                    console.log('not found: '+inlinePath);
+                    return;
+                }
+                // console.log('found: '+inlinePath);
+                res = prefix + new Buffer(fileData).toString('base64') + ')';
+            });
+
+            if (res) {
+                return res;
+            }
+            else {
+                return 'inline('+inlinePath+')';
+            }
         }
 
         // check if file.contents is a `Buffer`
         if (file.isBuffer()) {
-            var base64 = String(file.contents).replace(/inline\('(.+)'\)/g, inline);
+            var base64 = String(file.contents).replace(/inline\(([^\)]+)\)/g, inline);
             file.contents = new Buffer(base64);
 
             this.push(file);
